4bf6c1540de2097be4ea06b052abeb694e63f346
kfth <nieuwleven33@gmail.com>
2018-12-14 02:14:51 +0300

    rename week2 folder as week1

---
+++ week1/exercise4StringVariables.js
@@ -0,0 +1,18 @@
+//Exercise 4
+//4. Declare a variable y and assign a string to it.
+let y="This is the first string";
+
+//4.1 Write a console.log statement in which you explain in words what you think the value of the string is.
+console.log("the value of my string will be: This is the first string");
+
+//4.2 Now console.log the variable y.
+console.log("Value of string y is: "+ y);
+
+//4.3 Now assign a new string to the variable y.
+y="Javascript is awesome :)"
+
+//4.4 Just like what you did before write a console.log statement that explains in words what you think will be logged to the console.
+console.log("the value of my string will be: Javascript is awesome :)");
+
+//4.5 Now console.log y again.
+console.log("Value of string y is: "+ y);
